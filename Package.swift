edebca28210715cb0319bde262485479f86ab743
Scott Petit <scott@hecho.io>
2019-10-04 12:31:17 -0500

    Adds platforms to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Burgundy",
+    platforms: [
+        .macOS(.v10_12), .iOS(.v10), .tvOS(.v10)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
